84314758214b2299caf46b4e79844bb5898b4618
James <46839484+CodeMachine0121@users.noreply.github.com>
2023-08-04 00:51:01 +0800

    Update main.go

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ func main() {
 		handlers.Handle(w, r, bot)
 	})
 
-	if err := http.ListenAndServe(":80", nil); err != nil {
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
 }
